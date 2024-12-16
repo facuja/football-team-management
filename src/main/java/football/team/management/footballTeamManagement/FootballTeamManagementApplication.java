a495c295c645cfc7eb79ceccfe0deeba534848b3
facuja <57334395+facuja@users.noreply.github.com>
2024-12-16 20:20:35 -0300

    Primer commit

---
+++ src/main/java/football/team/management/footballTeamManagement/FootballTeamManagementApplication.java
@@ -0,0 +1,13 @@
+package football.team.management.footballTeamManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballTeamManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballTeamManagementApplication.class, args);
+	}
+
+}
